f900f3e7dba3db228621493479725967ba336337
Norbert B <dnorbertb@gmail.com>
2025-03-28 01:14:49 +0100

    Initial Commit

---
+++ NotesApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NotesApp
+//
+//  Created by Norbert Bednarczyk on 28/03/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
